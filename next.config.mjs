2e9af6da6585acb92794628bf659096fa97d8da1
taskercompany <taskercompany7@gmail.com>
2025-02-04 09:51:14 +0500

    Add PWA support with next-pwa for improved web app experience

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-          },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
         ],
-      },
+    }
 };
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+})(nextConfig);
